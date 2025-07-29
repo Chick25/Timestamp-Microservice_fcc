b5ee20f15a9f9171576992290eb87127d73d362e
Chick25 <145821030+Chick25@users.noreply.github.com>
2025-07-29 15:22:54 +0000

    done

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let input = req.params.date;
+  let date = isNaN(input) ? new Date(input) : new Date(Number(input));
+
+  if(date.toString() === 'Invalid Date'){
+    res.json({error : "Invalid Date"});
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toISOString()
+  })
 });
 
 
